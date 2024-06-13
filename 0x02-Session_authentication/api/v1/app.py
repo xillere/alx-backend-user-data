b47132c1b2eafc8eec4531865af7946936f8b35f
Chideraa <chideraaozobu@gmail.com>
2024-06-13 17:28:07 +0200

    part 2

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+if auth_type == 'session_auth':
+    auth = SessionAuth()
+if auth_type == 'session_exp_auth':
+    auth = SessionExpAuth()
+if auth_type == 'session_db_auth':
+    auth = SessionDBAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ HTTP status code for a request unauthorized?
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Error handler: Unauthorized
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Error handler: Forbidden
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def authenticate_user():
+    """filtering each request
+    """
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth.session_cookie(request) is None and \
+                    auth.authorization_header(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" class for authentication
+"""
+import os
+import re
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication
+       class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """identifying routes don't need authentication"""
+        if path is not None and excluded_paths is not None:
+            for ex_path in map(lambda x: x.strip(), excluded_paths):
+                sline = ''
+                if ex_path[-1] == '*':
+                    sline = '{}.*'.format(ex_path[0:-1])
+                elif ex_path[-1] == '/':
+                    sline = '{}/*'.format(ex_path[0:-1])
+                else:
+                    sline = '{}/*'.format(ex_path)
+                if re.match(sline, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """auth header drwan from req"""
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user is recieved from req"""
+        return None
+    
+    def session_cookie(self, request=None) -> str:
+        """retrieve value of the cookie named SESSION_NAME
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
